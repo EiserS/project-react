0070cd77d3cbb776f5de9aee0430bb31a3ae2984
Damian Pawlik <93448280+EiserS@users.noreply.github.com>
2022-12-02 19:56:59 +0100

    In progress v. 4.0

--- src/pages/SearchResults.tsx
+++ src/pages/SearchResults.tsx
@@ -2,23 +2,28 @@ import {useSearchParams} from "react-router-dom";
 
 export default function SearchResults() {
     const [searchParams] = useSearchParams();
+    const name = searchParams.get('name')
+    const minPrice = searchParams.get('minPrice')
+    const maxPrice = searchParams.get('maxPrice')
+    const category = searchParams.get('category')
+
     return (
         <>
             <h1>Name</h1>
             <p>
-                {searchParams.get('name')}
+                {name}
             </p>
             <h1>Min Price</h1>
             <p>
-                {searchParams.get('minPrice')}
+                {minPrice}
             </p>
             <h1>Max Price</h1>
             <p>
-                {searchParams.get('maxPrice')}
+                {maxPrice}
             </p>
             <h1>Category</h1>
             <p>
-                {searchParams.get('category')}
+                {category}
             </p>
         </>
     )
